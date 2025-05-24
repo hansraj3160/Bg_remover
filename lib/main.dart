12e37de0d7c6f94eef9cdec8151f63883b563d51
hansraj3160 <hansraj.bcsf18@iba-suk.edu.pk>
2025-05-24 21:50:02 +0530

    done

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Photo Bg remover',
+      title:'Photo Bg remover',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
